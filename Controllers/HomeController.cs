b16a49a77e789c717f066134ff7512e8c9108ac5
Ryan Lynch <rynplynch@gmail.com>
2024-06-02 17:23:19 -0700

    return secured view when hitting /home/secured

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,9 +23,16 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Secured()
+    {
+        return View((object)"Hello");
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(
+            new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
+        );
     }
 }
