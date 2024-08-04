a4d7dffb504be4b933ae8981c4c3690cebb3bea6
Ryan Lynch <rynplynch@gmail.com>
2024-08-04 15:59:46 -0700

    Removing experiments and bloat

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 using System.Diagnostics;
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SocialNexusClient.Models;
 
@@ -23,33 +21,6 @@ public class HomeController : Controller
     public IActionResult Privacy()
     {
         return View();
-    }
-
-    [Authorize]
-    public IActionResult Claims()
-    {
-        // extract the claims principal from the http context
-        // our controller passively passes on this data to the view
-        ClaimsPrincipal? p = HttpContext.User;
-
-        // render the view stored in the Claims.cshtml file
-        return View();
-    }
-
-    [Authorize]
-    public IActionResult Identities()
-    {
-        // extract the identities claims principle
-        IEnumerable<ClaimsIdentity> p = HttpContext.User.Identities;
-
-        // render the Identities.cshtml view
-        return View();
-    }
-
-    [Authorize]
-    public IActionResult Scopes()
-    {
-        return View((object)"TODO: present a users scopes here");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
