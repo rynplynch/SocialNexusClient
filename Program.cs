06a31a1940c89874bcda0cc608a62d328c69714b
Ryan Lynch <rynplynch@gmail.com>
2024-08-04 16:46:55 -0700

    started using Startup class

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using System.Net;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
+using SocialNexusClient.Services;
 
 namespace SocialNexusClient;
 
@@ -10,14 +17,36 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        CreateHostBuilder(args).Build().Run();
+    }
 
         // Add services to the container.
         builder
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
+
+public class Startup
+{
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
+    {
+        Configuration = configuration;
+        CurrentEnvironment = env;
+    }
+
+    private IWebHostEnvironment CurrentEnvironment { get; set; }
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services
             // default authentication scheme
-            .Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-            // pass configuration settings for SocialNexus AD B2C
-            .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
+            .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
 
         // configure web app to use MVC(model, view, controller) pattern
         builder.Services.AddControllersWithViews();
@@ -31,21 +60,40 @@ public class Program
 
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+        }
+        else
         {
             app.UseExceptionHandler("/Home/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
-
-        app.UseHttpsRedirection();
         app.UseStaticFiles();
+        app.UseCookiePolicy();
 
         app.UseRouting();
 
+        app.UseAuthentication();
         app.UseAuthorization();
 
-        app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
-        app.MapRazorPages();
+        app.UseStaticFiles();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
+            endpoints.MapRazorPages();
+        });
+    }
+
 
         app.Run();
     }
